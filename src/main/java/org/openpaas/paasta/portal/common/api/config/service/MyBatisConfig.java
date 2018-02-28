53835262f7a2958216a118fed20bc6a7ec450539
swmoon <swmoon@bluedigm.com>
2018-02-28 17:27:36 +0900

    Build.gradle 문제점 수정

--- src/main/java/org/openpaas/paasta/portal/common/api/config/service/MyBatisConfig.java
+++ src/main/java/org/openpaas/paasta/portal/common/api/config/service/MyBatisConfig.java
@@ -1,61 +1,48 @@
 package org.openpaas.paasta.portal.common.api.config.service;
 
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.annotation.MapperScan;
 //import org.openpaas.paasta.portal.api.config.service.surport.AutoScailing;
 //import org.openpaas.paasta.portal.api.config.service.surport.Cc;
 //import org.openpaas.paasta.portal.api.config.service.surport.Portal;
 //import org.openpaas.paasta.portal.api.config.service.surport.Uaa;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-
-import javax.sql.DataSource;
-import java.io.IOException;
 
 /**
  * Created by mg on 2016-05-20.
  */
 public class MyBatisConfig {
 
-    @Autowired
-    private Environment env;
-
-    @Value("${spring.jdbc}") String jdbcName;
-    @Value("${spring.datasource.portal.jdbc}") String portalJdbcName;
-
-
-
-    public static final String BASE_PACKAGE = "org.openpaas.paasta.portal.api";
-    public static final String MYSQL_MAPPER_LOCATIONS_PATH = "classpath:mapper/mysql/**/*.xml";
-    public static final String POSTGRES_MAPPER_LOCATIONS_PATH = "classpath:mapper/postgresql/**/*.xml";
-    public static final String AUTOSCAIL_MAPPER_LOCATIONS_PATH = "classpath:mapper/autoscail/**/*.xml";
-
-    protected void configureSqlSessionFactory(SqlSessionFactoryBean sessionFactoryBean, DataSource dataSource) throws IOException {
-        PathMatchingResourcePatternResolver pathResolver = new PathMatchingResourcePatternResolver();
-        sessionFactoryBean.setDataSource(dataSource);
-        jdbcName = portalJdbcName.isEmpty()?jdbcName:portalJdbcName;
-
-        if("mysql".equals(jdbcName)) {
-            sessionFactoryBean.setMapperLocations(pathResolver.getResources(MYSQL_MAPPER_LOCATIONS_PATH));
-        }
-        if("postgresql".equals(jdbcName)) {
-            sessionFactoryBean.setMapperLocations(pathResolver.getResources(POSTGRES_MAPPER_LOCATIONS_PATH));
-        }
-    }
-    //TODO
-    /**임시 삭제예정*/
-
-    protected void autoscailConfigureSqlSessionFactory(SqlSessionFactoryBean sessionFactoryBean, DataSource dataSource) throws IOException {
-        PathMatchingResourcePatternResolver pathResolver = new PathMatchingResourcePatternResolver();
-        sessionFactoryBean.setDataSource(dataSource);
-        sessionFactoryBean.setMapperLocations(pathResolver.getResources(AUTOSCAIL_MAPPER_LOCATIONS_PATH));
-    }
+//    @Autowired
+//    private Environment env;
+//
+//    @Value("${spring.jdbc}") String jdbcName;
+//    @Value("${spring.datasource.portal.jdbc}") String portalJdbcName;
+//
+//
+//
+//    public static final String BASE_PACKAGE = "org.openpaas.paasta.portal.api";
+//    public static final String MYSQL_MAPPER_LOCATIONS_PATH = "classpath:mapper/mysql/**/*.xml";
+//    public static final String POSTGRES_MAPPER_LOCATIONS_PATH = "classpath:mapper/postgresql/**/*.xml";
+//    public static final String AUTOSCAIL_MAPPER_LOCATIONS_PATH = "classpath:mapper/autoscail/**/*.xml";
+//
+//    protected void configureSqlSessionFactory(SqlSessionFactoryBean sessionFactoryBean, DataSource dataSource) throws IOException {
+//        PathMatchingResourcePatternResolver pathResolver = new PathMatchingResourcePatternResolver();
+//        sessionFactoryBean.setDataSource(dataSource);
+//        jdbcName = portalJdbcName.isEmpty()?jdbcName:portalJdbcName;
+//
+//        if("mysql".equals(jdbcName)) {
+//            sessionFactoryBean.setMapperLocations(pathResolver.getResources(MYSQL_MAPPER_LOCATIONS_PATH));
+//        }
+//        if("postgresql".equals(jdbcName)) {
+//            sessionFactoryBean.setMapperLocations(pathResolver.getResources(POSTGRES_MAPPER_LOCATIONS_PATH));
+//        }
+//    }
+//    //TODO
+//    /**임시 삭제예정*/
+//
+//    protected void autoscailConfigureSqlSessionFactory(SqlSessionFactoryBean sessionFactoryBean, DataSource dataSource) throws IOException {
+//        PathMatchingResourcePatternResolver pathResolver = new PathMatchingResourcePatternResolver();
+//        sessionFactoryBean.setDataSource(dataSource);
+//        sessionFactoryBean.setMapperLocations(pathResolver.getResources(AUTOSCAIL_MAPPER_LOCATIONS_PATH));
+//    }
 }
 
 //@Configuration
