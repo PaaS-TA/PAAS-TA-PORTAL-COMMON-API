ad861f1897cbd29bd68dcbcc18de6bcb6e3b0807
betterpoul <betterpoul>
2018-03-05 17:02:05 +0900

    test

--- src/main/java/org/openpaas/paasta/portal/common/api/domain/adminMain/AdminMainService.java
+++ src/main/java/org/openpaas/paasta/portal/common/api/domain/adminMain/AdminMainService.java
@@ -6,14 +6,12 @@ import org.openpaas.paasta.portal.common.api.config.dataSource.CcConfig;
 import org.openpaas.paasta.portal.common.api.entity.cc.OrganizationsCc;
 import org.openpaas.paasta.portal.common.api.entity.cc.OrganizationsTolCc;
 import org.openpaas.paasta.portal.common.api.entity.cc.SpacesCc;
-import org.openpaas.paasta.portal.common.api.entity.cc.SpaceunionViewCc;
+import org.openpaas.paasta.portal.common.api.repository.cc.SpacesCcRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Tuple;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.*;
+import javax.persistence.Query;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,6 +25,9 @@ public class AdminMainService {
 
     @Autowired
     CcConfig ccConfig;
+
+    @Autowired
+    SpacesCcRepository spacesCcRepository;
 
     @Autowired
     JinqSource jinqSource;
@@ -71,86 +72,94 @@ public class AdminMainService {
     }
 
     public Map<String, Object>getTotalSpaceList(Map<String, Object> reqParam) {
-//        JinqStream<SpacesCc> streams = jinqSource.streamAllCc(SpacesCc.class);
-//
-//        if(null != reqParam.get("organizationId")) {
-//            int id = Integer.parseInt(reqParam.get("organizationId").toString());
-//            streams = streams.where(c -> c.getOrganizationId() == id);
-//        }
-
-
         EntityManager ccEm = ccConfig.ccEntityManager().getNativeEntityManagerFactory().createEntityManager();
 
-        CriteriaBuilder cb = ccEm.getCriteriaBuilder();
-        CriteriaQuery<Tuple> cq = cb.createTupleQuery();
-        Root<SpacesCc> from = cq.from(SpacesCc.class);
-
-        cq.multiselect(from.get("id").alias("spaceId")
-                , from.get("name").alias("spaceName"));
-
-        Predicate predicate = cb.conjunction();
-
+//        CriteriaBuilder cb = ccEm.getCriteriaBuilder();
+//        CriteriaQuery<Tuple> cq = cb.createTupleQuery();
+//        Root<SpacesCc> from = cq.from(SpacesCc.class);
+//
+//        cq.multiselect(from.get("id").alias("spaceId")
+//                , from.get("name").alias("spaceName"));
+//
+//        Predicate predicate = cb.conjunction();
+//
+//        if(null != reqParam.get("organizationId") && !("").equals(reqParam.get("organizationId").toString())) {
+//            predicate = cb.and(predicate, cb.equal(from.get("organizationId"), reqParam.get("organizationId").toString()));
+//        }
+//
+//        cq.where(predicate);
+//
+//
+//        TypedQuery<Tuple> tq = ccEm.createQuery(cq);
+//        List<Tuple> resultList = tq.getResultList();
+//
+//        CriteriaQuery<Tuple> cq2 = cb.createTupleQuery();
+//        Root<SpaceunionViewCc> from2 = cq2.from(SpaceunionViewCc.class);
+//        cq2.multiselect(from2.get("spaceId").alias("spaceId")
+//                , from2.get("applicationCount").alias("applicationCount")
+//                , from2.get("spaceName").alias("spaceName")).distinct(true);
+//
+//        if(null != reqParam.get("organizationId") && !("").equals(reqParam.get("organizationId").toString())) {
+//            Subquery<SpacesCc> subQuery = cq2.subquery(SpacesCc.class);
+//            Root<SpaceunionViewCc> subM = subQuery.correlate(from2);
+//            Root<SpacesCc> subFrom = subQuery.from(SpacesCc.class);
+//
+//            subQuery.select(subFrom.get("id"));
+//
+//            Predicate predicate2 = cb.conjunction();
+//            predicate2 = cb.and(predicate2, cb.equal(subFrom.get("id"), subM.get("spaceId")));
+//            predicate2 = cb.and(predicate2, cb.equal(subFrom.get("organizationId"), reqParam.get("organizationId").toString()));
+//
+//            subQuery.where(predicate2);
+//
+//            cq2.where(cb.exists(subQuery));
+//            cq2.groupBy(from2.get("spaceId"));
+//        }
+//
+//        TypedQuery<Tuple> tq2 = ccEm.createQuery(cq2);
+//        List<Tuple> resultList2 = tq2.getResultList();
+//
+//        List<Map<String, Object>> resultList1f = resultList.stream().map(x -> new HashMap<String, Object>(){{
+//            put("spaceId", x.get("spaceId"));
+//            put("spaceName", x.get("spaceName"));
+//        }}).collect(Collectors.toList());
+//
+//        List<Map<String, Object>> resultList2f = resultList2.stream().map(x -> new HashMap<String, Object>(){{
+//            put("spaceId", x.get("spaceId"));
+//            put("applicationCount", x.get("applicationCount"));
+//            put("spaceName", x.get("spaceName"));
+//        }}).collect(Collectors.toList());
+//
+//        for(int i=0; i<resultList1f.size(); i++) {
+//            for(int j=0; j<resultList2f.size(); j++) {
+//                if(resultList2f.get(j).get("spaceId") == resultList1f.get(i).get("spaceId")) {
+//                    resultList1f.get(i).put("applicationCount", resultList2f.get(j).get("applicationCount"));
+//                    break;
+//                } else {
+//                    resultList1f.get(i).put("applicationCount", 0);
+//                }
+//            }
+//        }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        Query q = null;
         if(null != reqParam.get("organizationId") && !("").equals(reqParam.get("organizationId").toString())) {
-            predicate = cb.and(predicate, cb.equal(from.get("organizationId"), reqParam.get("organizationId").toString()));
+            q = ccEm.createNamedQuery("SpacesCc.allByOrganizationIdList");
+            q.setParameter(1, reqParam.get("organizationId"));
+            q.setParameter(2, reqParam.get("organizationId"));
+        } else {
+            q = ccEm.createNamedQuery("SpacesCc.allList");
         }
 
-        cq.where(predicate);
+        List<SpacesCc> results = q.getResultList();
 
-
-        TypedQuery<Tuple> tq = ccEm.createQuery(cq);
-        List<Tuple> resultList = tq.getResultList();
-
-        CriteriaQuery<Tuple> cq2 = cb.createTupleQuery();
-        Root<SpaceunionViewCc> from2 = cq2.from(SpaceunionViewCc.class);
-        cq2.multiselect(from2.get("spaceId").alias("spaceId")
-                , from2.get("applicationCount").alias("applicationCount")
-                , from2.get("spaceName").alias("spaceName")).distinct(true);
-
-        if(null != reqParam.get("organizationId") && !("").equals(reqParam.get("organizationId").toString())) {
-            Subquery<SpacesCc> subQuery = cq2.subquery(SpacesCc.class);
-            Root<SpaceunionViewCc> subM = subQuery.correlate(from2);
-            Root<SpacesCc> subFrom = subQuery.from(SpacesCc.class);
-
-            subQuery.select(subFrom.get("id"));
-
-            Predicate predicate2 = cb.conjunction();
-            predicate2 = cb.and(predicate2, cb.equal(subFrom.get("id"), subM.get("spaceId")));
-            predicate2 = cb.and(predicate2, cb.equal(subFrom.get("organizationId"), reqParam.get("organizationId").toString()));
-
-            subQuery.where(predicate2);
-
-            cq2.where(cb.exists(subQuery));
-            cq2.groupBy(from2.get("spaceId"));
-        }
-
-        TypedQuery<Tuple> tq2 = ccEm.createQuery(cq2);
-        List<Tuple> resultList2 = tq2.getResultList();
-
-        List<Map<String, Object>> resultList1f = resultList.stream().map(x -> new HashMap<String, Object>(){{
-            put("spaceId", x.get("spaceId"));
-            put("spaceName", x.get("spaceName"));
+        List<Map<String, Object>> resultList = results.stream().map(x -> new HashMap<String, Object>(){{
+            put("spaceId", x.getId());
+            put("applicationCount", x.getApplicationCount());
+            put("spaceName", x.getName());
         }}).collect(Collectors.toList());
-
-        List<Map<String, Object>> resultList2f = resultList2.stream().map(x -> new HashMap<String, Object>(){{
-            put("spaceId", x.get("spaceId"));
-            put("applicationCount", x.get("applicationCount"));
-            put("spaceName", x.get("spaceName"));
-        }}).collect(Collectors.toList());
-
-        for(int i=0; i<resultList1f.size(); i++) {
-            for(int j=0; j<resultList2f.size(); j++) {
-                if(resultList2f.get(j).get("spaceId") == resultList1f.get(i).get("spaceId")) {
-                    resultList1f.get(i).put("applicationCount", resultList2f.get(j).get("applicationCount"));
-                    break;
-                } else {
-                    resultList1f.get(i).put("applicationCount", 0);
-                }
-            }
-        }
 
         return new HashMap<String, Object>() {{
-            put("list", resultList1f);
+            put("list", resultList);
         }};
-//        return null;
     }
 }
--- src/main/java/org/openpaas/paasta/portal/common/api/entity/cc/SpacesCc.java
+++ src/main/java/org/openpaas/paasta/portal/common/api/entity/cc/SpacesCc.java
@@ -1,56 +1,111 @@
 package org.openpaas.paasta.portal.common.api.entity.cc;
 
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
 import javax.persistence.*;
-import java.util.Date;
 
 /**
  * Created by indra on 2018-02-13.
  */
 @Entity
+@NamedNativeQueries({
+    @NamedNativeQuery(name = "SpacesCc.allList",
+            query = "SELECT " +
+                    "    space_1.id, " +
+                    "    space_2.applicationCount, " +
+                    "    space_1.name " +
+                    "FROM " +
+                    "(SELECT * FROM spaces s1 WHERE 1=1" +
+                    ") space_1 " +
+                    "LEFT OUTER JOIN " +
+                    "(SELECT " +
+                    "    DISTINCT (space_all.space_id) AS spaceId, " +
+                    "    (SELECT COUNT(a.id) FROM apps a WHERE a.id = space_all.space_id) AS applicationCount, " +
+                    "    (SELECT s2.name FROM spaces s2 WHERE s2.id = space_all.space_id) AS spaceName " +
+                    "FROM " +
+                    "(SELECT * FROM spaces_auditors " +
+                    "    UNION ALL " +
+                    "SELECT * FROM spaces_developers " +
+                    "    UNION ALL " +
+                    "SELECT * FROM spaces_managers) space_all " +
+                    "WHERE 1=1 " +
+                    ") space_2 " +
+                    "ON space_1.id = space_2.spaceId " +
+                    "ORDER BY space_1.id ASC",
+            resultClass = SpacesCc.class
+    ),
+    @NamedNativeQuery(name = "SpacesCc.allByOrganizationIdList",
+            query = "SELECT " +
+                    "    space_1.id, " +
+                    "    space_2.applicationCount, " +
+                    "    space_1.name " +
+                    "FROM " +
+                    "(SELECT * FROM spaces s1 WHERE 1=1 " +
+                    "AND s1.organization_id = ? " +
+                    ") space_1 " +
+                    "LEFT OUTER JOIN " +
+                    "(SELECT " +
+                    "    DISTINCT (space_all.space_id) AS spaceId, " +
+                    "    (SELECT COUNT(a.id) FROM apps a WHERE a.id = space_all.space_id) AS applicationCount, " +
+                    "    (SELECT s2.name FROM spaces s2 WHERE s2.id = space_all.space_id) AS spaceName " +
+                    "FROM " +
+                    "(SELECT * FROM spaces_auditors " +
+                    "    UNION ALL " +
+                    "SELECT * FROM spaces_developers " +
+                    "    UNION ALL " +
+                    "SELECT * FROM spaces_managers) space_all " +
+                    "WHERE 1=1 " +
+                    "   AND EXISTS " +
+                    "   (SELECT s.id FROM spaces s WHERE s.id = space_all.space_id AND s.organization_id = ?) " +
+                    "   GROUP BY space_all.space_id " +
+                    ") space_2 " +
+                    "ON space_1.id = space_2.spaceId " +
+                    "ORDER BY space_1.id ASC",
+            resultClass = SpacesCc.class
+    )
+})
 @Table(name = "spaces")
 public class SpacesCc {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private int id;
+    private Integer id;
 
-    @Column(name = "guid", nullable = false)
-    private String guid;
-
-    @CreationTimestamp
-    @Column(name = "created_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP", nullable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdAt;
-
-    @UpdateTimestamp
-    @Column(name = "updated_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date updatedAt;
-
+//    @Column(name = "guid", nullable = false)
+//    private String guid;
+//
+//    @CreationTimestamp
+//    @Column(name = "created_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP", nullable = false)
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date createdAt;
+//
+//    @UpdateTimestamp
+//    @Column(name = "updated_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date updatedAt;
+//
     @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name = "organization_id", nullable = false)
-    private int organizationId;
+    private Integer applicationCount;
+//
+//    @Column(name = "organization_id", nullable = false)
+//    private Integer organizationId;
+//
+//    @Column(name = "space_quota_definition_id")
+//    private Integer spaceQuotaDefinitionId;
 
-    @Column(name = "space_quota_definition_id")
-    private int spaceQuotaDefinitionId;
+//    @Column(name = "allow_ssh", columnDefinition = "TINYINT", length = 1)
+//    @Type(type = "org.hibernate.type.NumericBooleanType")
+//    private Boolean allowSsh;
 
-    @Column(name = "allow_ssh", columnDefinition="boolean default true")
-    private Boolean allowSsh;
-
-    @Column(name = "isolation_segment_guid")
-    private String isolationSegmentGuid;
-
-    @Column(name = "space_id")
-    private int spaceId;
-
-    @Column(name = "user_id")
-    private int userId;
+//    @Column(name = "isolation_segment_guid")
+//    private String isolationSegmentGuid;
+//
+//    @Column(name = "space_id")
+//    private Integer spaceId;
+//
+//    @Column(name = "user_id")
+//    private Integer userId;
 
 
     public int getId() {
@@ -61,29 +116,29 @@ public class SpacesCc {
         this.id = id;
     }
 
-    public String getGuid() {
-        return guid;
-    }
-
-    public void setGuid(String guid) {
-        this.guid = guid;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Date getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
-    }
+//    public String getGuid() {
+//        return guid;
+//    }
+//
+//    public void setGuid(String guid) {
+//        this.guid = guid;
+//    }
+//
+//    public Date getCreatedAt() {
+//        return createdAt;
+//    }
+//
+//    public void setCreatedAt(Date createdAt) {
+//        this.createdAt = createdAt;
+//    }
+//
+//    public Date getUpdatedAt() {
+//        return updatedAt;
+//    }
+//
+//    public void setUpdatedAt(Date updatedAt) {
+//        this.updatedAt = updatedAt;
+//    }
 
     public String getName() {
         return name;
@@ -93,51 +148,59 @@ public class SpacesCc {
         this.name = name;
     }
 
-    public int getOrganizationId() {
-        return organizationId;
+    public Integer getApplicationCount() {
+        return applicationCount;
     }
 
-    public void setOrganizationId(int organizationId) {
-        this.organizationId = organizationId;
+    public void setApplicationCount(Integer applicationCount) {
+        this.applicationCount = applicationCount;
     }
 
-    public int getSpaceQuotaDefinitionId() {
-        return spaceQuotaDefinitionId;
-    }
+    //    public int getOrganizationId() {
+//        return organizationId;
+//    }
+//
+//    public void setOrganizationId(int organizationId) {
+//        this.organizationId = organizationId;
+//    }
+//
+//    public int getSpaceQuotaDefinitionId() {
+//        return spaceQuotaDefinitionId;
+//    }
+//
+//    public void setSpaceQuotaDefinitionId(int spaceQuotaDefinitionId) {
+//        this.spaceQuotaDefinitionId = spaceQuotaDefinitionId;
+//    }
 
-    public void setSpaceQuotaDefinitionId(int spaceQuotaDefinitionId) {
-        this.spaceQuotaDefinitionId = spaceQuotaDefinitionId;
-    }
+//    public Boolean getAllowSsh() {
+//        return allowSsh;
+//    }
+//
+//    public void setAllowSsh(Boolean allowSsh) {
+//        this.allowSsh = allowSsh;
+//    }
 
-    public Boolean getAllowSsh() {
-        return allowSsh;
-    }
+//    public String getIsolationSegmentGuid() {
+//        return isolationSegmentGuid;
+//    }
+//
+//    public void setIsolationSegmentGuid(String isolationSegmentGuid) {
+//        this.isolationSegmentGuid = isolationSegmentGuid;
+//    }
 
-    public void setAllowSsh(Boolean allowSsh) {
-        this.allowSsh = allowSsh;
-    }
+//    public int getSpaceId() {
+//        return spaceId;
+//    }
+//
+//    public void setSpaceId(int spaceId) {
+//        this.spaceId = spaceId;
+//    }
 
-    public String getIsolationSegmentGuid() {
-        return isolationSegmentGuid;
-    }
-
-    public void setIsolationSegmentGuid(String isolationSegmentGuid) {
-        this.isolationSegmentGuid = isolationSegmentGuid;
-    }
-
-    public int getSpaceId() {
-        return spaceId;
-    }
-
-    public void setSpaceId(int spaceId) {
-        this.spaceId = spaceId;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
+//    public int getUserId() {
+//        return userId;
+//    }
+//
+//    public void setUserId(int userId) {
+//        this.userId = userId;
+//    }
 }
