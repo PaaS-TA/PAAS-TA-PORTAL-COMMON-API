e31f1f9b94d333223e55fe7f67c018b3a76477ae
swmoon <swmoon@bluedigm.com>
2018-03-20 13:59:37 +0900

    PortalAdmin - 메뉴 수정

--- src/main/java/org/openpaas/paasta/portal/common/api/domain/configInfo/ConfigInfoController.java
+++ src/main/java/org/openpaas/paasta/portal/common/api/domain/configInfo/ConfigInfoController.java
@@ -3,11 +3,10 @@ package org.openpaas.paasta.portal.common.api.domain.configInfo;
 import org.openpaas.paasta.portal.common.api.entity.portal.ConfigInfo;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.ws.rs.DELETE;
+import javax.ws.rs.PathParam;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,28 +28,54 @@ public class ConfigInfoController {
     /**
      * 설정 정보 값을 조회한다.
      *
-     * @param configInfo the config info
+     * @param name - - primary key, configInfo the config info
      * @return value value
      * @throws Exception the exception
      */
-    @RequestMapping(value = {"/getValue"}, method = RequestMethod.POST)
-    public Map<String, Object> getValue(@RequestBody ConfigInfo configInfo) {
+    @GetMapping("/{name:.+}")
+    public Map<String, Object> getValue(@PathVariable String name, @RequestBody ConfigInfo configInfo) {
         return configInfoService.getValue(configInfo);
     }
 
     /**
-     * 설정 정보 값을 수정한다.
+     * 설정 정보 값을 생성한다..
      *
-     * @param configInfo the config info
+     * @param configInfo -
      * @return map map
      * @throws Exception the exception
      */
-    @RequestMapping(value = {"/updateValue"}, method = RequestMethod.POST)
-    public Map<String, Object> updateValue(@RequestBody ConfigInfo configInfo) {
-        Map<String, Object> resultMap = new HashMap<>();
+    @PostMapping()
+    public Map<String, Object> insertValue(@RequestBody ConfigInfo configInfo) {
+        return null;
+    }
 
+
+    /**
+     * 설정 정보 값을 수정한다.
+     *
+     * @param name - primary key, configInfo the config info
+     * @return map map
+     * @throws Exception the exception
+     */
+    @PutMapping(value = {"/{name:.+}"})
+    public Map<String, Object> updateValue(@PathVariable String name, @RequestBody ConfigInfo configInfo) {
+        Map<String, Object> resultMap = new HashMap<>();
         String resultStr = configInfoService.updateValue(configInfo);
         resultMap.put("RESULT", resultStr);
         return resultMap;
     }
+
+    /**
+     * 설정 정보 값을 삭제한다.
+     *
+     * @param name - primary key
+     * @return map map
+     * @throws Exception the exception
+     */
+    @DeleteMapping(value = {"/{name:.+}"})
+    public Map<String, Object> deleteValue(@PathVariable String name) {
+        return null;
+    }
+
+
 }
