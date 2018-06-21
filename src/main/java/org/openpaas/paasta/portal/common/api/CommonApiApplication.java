fb7fe28bed22471b1dd624c4767f38fbc7cea01d
swmoon <swmoon@bluedigm.com>
2018-06-21 16:54:39 +0900

    불필요 주석 제거

--- src/main/java/org/openpaas/paasta/portal/common/api/CommonApiApplication.java
+++ src/main/java/org/openpaas/paasta/portal/common/api/CommonApiApplication.java
@@ -5,26 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
-/**
- * All you need to run a Eureka registration server.
- * 
- * @author Paul Chapman
- */
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @ComponentScan(basePackages = {"org.openpaas.paasta.portal.common.api"})
 public class CommonApiApplication {
-
-	/**
-	 * Run the application using Spring Boot and an embedded servlet engine.
-	 * 
-	 * @param args
-	 *            Program arguments - ignored.
-	 */
-	public static void main(String[] args) {
-		// Tell server to look for registration.properties or registration.yml
-
-		SpringApplication.run(CommonApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CommonApiApplication.class, args);
+    }
 
 }
