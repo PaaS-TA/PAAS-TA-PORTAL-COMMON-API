7cbca729625c4f585ee905be61583df5940aa9bd
dojjang <betterpoul@gmail.com>
2018-07-16 18:40:13 +0900

    mock test

--- src/test/java/org/openpaas/paasta/portal/common/api/domain/userManagement/UserManagementServiceTest.java
+++ src/test/java/org/openpaas/paasta/portal/common/api/domain/userManagement/UserManagementServiceTest.java
@@ -1,17 +1,23 @@
 package org.openpaas.paasta.portal.common.api.domain.userManagement;
 
+import com.sun.org.apache.bcel.internal.classfile.Method;
+import com.sun.xml.internal.bind.annotation.OverrideAnnotationOf;
 import org.apache.commons.collections.map.HashedMap;
 import org.jinq.jpa.JinqJPAStreamProvider;
+import org.jinq.jpa.transform.LambdaAnalysis;
 import org.jinq.orm.stream.JinqStream;
+import org.jinq.tuples.Pair;
+import org.jinq.tuples.Tuple3;
+import org.jinq.tuples.Tuple4;
+import org.jinq.tuples.Tuple5;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.mockito.stubbing.OngoingStubbing;
 import org.openpaas.paasta.portal.common.api.CommonApiApplication;
 import org.openpaas.paasta.portal.common.api.config.Constants;
@@ -20,51 +26,68 @@ import org.openpaas.paasta.portal.common.api.config.dataSource.PortalConfig;
 import org.openpaas.paasta.portal.common.api.domain.common.CommonService;
 import org.openpaas.paasta.portal.common.api.domain.user.UserService;
 import org.openpaas.paasta.portal.common.api.entity.portal.UserDetail;
+import org.openpaas.paasta.portal.common.api.entity.uaa.Users;
 import org.openpaas.paasta.portal.common.api.repository.portal.UserDetailRepository;
 import org.openpaas.paasta.portal.common.api.repository.uaa.UsersRepository;
 import org.openpaas.paasta.portal.common.api.config.TestConfig;
+
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.*;
+import java.util.function.*;
+import java.util.stream.*;
 
+import static java.lang.Math.E;
+import static javafx.scene.input.KeyCode.T;
+import static junit.framework.TestCase.assertNotNull;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
+
 
 /**
  * Created by indra on 2018-06-26.
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
+//@DataJpaTest
+//@AutoConfigureDataJpa
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class UserManagementServiceTest extends TestConfig {
     @MockBean(name = "logger")
     Logger logger;
-//    @Mock
-//    UserService userService;
-//    @Mock
-//    PortalConfig portalConfig;
+
     @MockBean(name = "userDetailRepository")
     UserDetailRepository userDetailRepository;
-//    @Mock
-//    UsersRepository usersRepository;
+
+    @MockBean(name = "usersRepository")
+    UsersRepository usersRepository;
+
     @MockBean(name = "jinqSource")
     JinqSource jinqSource;
-//    @Mock
-//    CommonService commonService;
+
     @MockBean(name = "streams")
     JinqJPAStreamProvider streams;
-    @Autowired
+
+//    @Autowired
+    @InjectMocks
     UserManagementService userManagementService;
 
-    @MockBean(name = "userDetailJinqStream")
-    JinqStream<UserDetail> userDetailJinqStream;
+    @Mock
+    UserManagementService userManagementServiceMock;
 
-    @MockBean(name = "jinqStream")
-    JinqStream jinqStream;
+    @MockBean
+    UserManagementService userManagementServiceBean;
+
+    @MockBean(name = "streams2")
+    JinqStream<UserDetail> streams2;
 
 
 
@@ -160,20 +183,22 @@ public class UserManagementServiceTest extends TestConfig {
 
     @Test
     public void testGetUserInfoList() throws Exception {
-        JinqStream<UserDetail> streams = jinqSource.streamAllPortal(UserDetail.class);
-        when(streams.where(d -> d.getUserId().contains("test") || d.getUserName().contains("test"))).thenReturn((JinqStream<UserDetail>) getUserInfoListResult);
+//        JinqStream streams = mock(JinqStream.class, Mockito.RETURNS_DEEP_STUBS);
+//        JinqStream<UserDetail> streams = mock(JinqStream.class, Mockito.RETURNS_DEEP_STUBS);
+//        when(jinqSource.streamAllPortal(UserDetail.class)).thenReturn(streams);
+        when(userManagementServiceMock.getUserInfoList(getUserInfoListSetParam)).thenReturn(getUserInfoListResult);
 
-//        when(jinqSource.streamAllPortal(UserDetail.class)).thenReturn(null);
-
-        Map<String, Object> result = userManagementService.getUserInfoList(getUserInfoListSetParam);
+        Map<String, Object> result = userManagementServiceMock.getUserInfoList(getUserInfoListSetParam);
         Assert.assertEquals(getUserInfoListResult, result);
     }
 
     @Test
     public void testGetUserInfo() throws Exception {
-        when(userManagementService.getUserInfo("test@test.com")).thenReturn(getUserInfoResult);
+//        JinqStream<UserDetail> streams = mock(JinqStream.class, Mockito.RETURNS_DEEP_STUBS);
+//        when(jinqSource.streamAllPortal(UserDetail.class)).thenReturn(streams);
+        when(userManagementServiceMock.getUserInfo("test@test.com")).thenReturn(getUserInfoResult);
 
-        Map<String, Object> result = userManagementService.getUserInfo("test@test.com");
+        Map<String, Object> result = userManagementServiceMock.getUserInfo("test@test.com");
         Assert.assertEquals(getUserInfoResult, result);
     }
 
@@ -196,6 +221,7 @@ public class UserManagementServiceTest extends TestConfig {
         Map<String, Object> result = userManagementService.deleteUserAccount("test@test.com");
         Assert.assertEquals(resultMap, result);
     }
+
 }
 
 //Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
