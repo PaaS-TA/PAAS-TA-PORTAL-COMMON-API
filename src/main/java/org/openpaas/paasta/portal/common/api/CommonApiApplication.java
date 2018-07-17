37a8508b166fb81aae66e23a5f672b325627c286
박철한 <parkcheolhan@bluedigm.com>
2018-07-17 13:25:27 +0900

    datasource 수정

--- src/main/java/org/openpaas/paasta/portal/common/api/CommonApiApplication.java
+++ src/main/java/org/openpaas/paasta/portal/common/api/CommonApiApplication.java
@@ -1,9 +1,7 @@
 package org.openpaas.paasta.portal.common.api;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,7 +10,6 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableCircuitBreaker
 @EnableDiscoveryClient
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 @ComponentScan(basePackages = {"org.openpaas.paasta.portal.common.api"})
 public class CommonApiApplication {
     public static void main(String[] args) {
