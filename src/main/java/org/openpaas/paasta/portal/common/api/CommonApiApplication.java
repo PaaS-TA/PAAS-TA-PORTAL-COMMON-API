69c22d8d73211e4bc6eec1524e392a6ba76bb491
박철한 <parkcheolhan@bluedigm.com>
2018-07-17 13:19:12 +0900

    datasource 정의

--- src/main/java/org/openpaas/paasta/portal/common/api/CommonApiApplication.java
+++ src/main/java/org/openpaas/paasta/portal/common/api/CommonApiApplication.java
@@ -1,7 +1,9 @@
 package org.openpaas.paasta.portal.common.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,6 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableCircuitBreaker
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 @ComponentScan(basePackages = {"org.openpaas.paasta.portal.common.api"})
 public class CommonApiApplication {
     public static void main(String[] args) {
